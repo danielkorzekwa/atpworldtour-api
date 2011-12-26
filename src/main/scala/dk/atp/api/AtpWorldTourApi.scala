ed7a4de40e700b35dc291f6570725ee5e350b6d6
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2011-12-26 20:14:04 +0000

    Adding MatchFactEnum

--- src/main/scala/dk/atp/api/AtpWorldTourApi.scala
+++ src/main/scala/dk/atp/api/AtpWorldTourApi.scala
@@ -1,8 +1,19 @@
 package dk.atp.api
 
-/**API interface for atpworldtour.com tennis statistics.
- * 
+/**
+ * API interface for atpworldtour.com tennis statistics.
+ *
  */
 trait AtpWorldTourApi {
 
+  object MatchfactEnum extends Enumeration {
+    type MatchFactEnum = Value
+    val FIRST_SERVE = Value("1")
+    val FIRST_SERVE_POINTS_WON = Value("2")
+    val SECOND_SERVE_POINTS_WON = Value("3")
+    val FIRST_SERVE_RETURN_POINTS_WON = Value("6")
+    val SECOND_SERVE_RETURN_POINTS_WON = Value("7")
+
+    override def toString() = MatchfactEnum.values.mkString("MatchfactEnum [", ", ", "]")
+  }
 }
