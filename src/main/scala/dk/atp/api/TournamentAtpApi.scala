8de1e7a4c66688500d08acaf118e43d2169bb972
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2012-02-10 20:14:10 +0000

    Adding utility methods.

--- src/main/scala/dk/atp/api/TournamentAtpApi.scala
+++ src/main/scala/dk/atp/api/TournamentAtpApi.scala
@@ -14,11 +14,33 @@ object TournamentAtpApi {
   /**
    * @param numOfSet 2 (THREE_SET_MATCH) or 3 (FIVE_SET_MATCH)
    */
-  case class Tournament(tournamentTime:Date,tournamentName:String, surface: SurfaceEnum, numOfSet:Int, tournamentUrl:String)
-  case class Match(score: String, matchFactsUrl:String)
-  
-  case class MatchFacts(playerAFacts:PlayerFacts,playerBFacts:PlayerFacts, winner:String, round:String, durationMinutes: Int)
-  case class PlayerFacts(playerName:String,totalServicePoints:Int,totalServicePointsWon:Int)
+  case class Tournament(tournamentTime: Date, tournamentName: String, surface: SurfaceEnum, numOfSet: Int, tournamentUrl: String)
+  case class Match(score: String, matchFactsUrl: String)
+
+  case class MatchFacts(playerAFacts: PlayerFacts, playerBFacts: PlayerFacts, winner: String, round: String, durationMinutes: Int) {
+
+    def playerFacts(playerName: String): Option[PlayerFacts] = {
+      val facts = playerName match {
+        case playerAFacts.playerName => Option(playerAFacts)
+        case playerBFacts.playerName => Option(playerBFacts)
+        case _ => None
+      }
+      facts
+    }
+
+    def playerOpponentFacts(playerName: String): Option[PlayerFacts] = {
+      val facts = playerName match {
+        case playerAFacts.playerName => Option(playerBFacts)
+        case playerBFacts.playerName => Option(playerAFacts)
+        case _ => None
+      }
+      facts
+    }
+
+    def containsPlayer(playerName: String): Boolean = (playerAFacts.playerName :: playerBFacts.playerName :: Nil).contains(playerName)
+  }
+
+  case class PlayerFacts(playerName: String, totalServicePointsWon: Int, totalServicePoints: Int)
 }
 
 trait TournamentAtpApi {
@@ -27,13 +49,13 @@ trait TournamentAtpApi {
    * @param year
    */
   def parseTournaments(year: Int): List[Tournament]
-  
+
   /**
    * @param tournamentUrl, e.g. http://www.atpworldtour.com/Share/Event-Draws.aspx?e=580&y=2011
-   * 
+   *
    */
-  def parseTournament(tournamentUrl: String) :List[Match]
-  
+  def parseTournament(tournamentUrl: String): List[Match]
+
   /**
    * @param url e.g. http://www.atpworldtour.com/Share/Match-Facts-Pop-Up.aspx?t=580&y=2011&r=1&p=N409
    */
