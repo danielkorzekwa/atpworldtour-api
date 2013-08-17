db337698748c8c33c2edcb79ea82ef5a11cd53c3
Dinesh Weerapurage <xydinesh@gmail.com>
2013-08-16 21:54:46 -0400

    adding sbt file

--- project/Build.scala
+++ project/Build.scala
@@ -1,34 +1,31 @@
 import sbt._
 import sbt.Keys._
 
-object Neo4jScalaBuild extends Build {
+object AtpWorldTourScalaBuild extends Build {
 
-  val neo4jVersion = "1.9"
-  val neo4jShellVersion = neo4jVersion
-  val neo4jCypherVersion = neo4jVersion
-  val neo4jRestVersion = neo4jVersion
+  val AtpWorldTourVersion = "4.0.0"
+  val AtpWorldTourShellVersion = AtpWorldTourVersion
+  val AtpWorldTourCypherVersion = AtpWorldTourVersion
+  val AtpWorldTourRestVersion = AtpWorldTourVersion
 
 
-  lazy val neo4jScala = Project(
-    id = "neo4j-scala-south",
+  lazy val AtpWorldTourScala = Project(
+    id = "atpworldtour-api",
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
-      name := "Neo4j Scala South",
+      name := "AtpWorldTour",
       fork := true,
-      organization := "org.neo4j",
-      version := "0.0.1",
+      organization := "org.atpworldtour",
+      version := "0.2",
       scalaVersion := "2.9.2",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       resolvers += "Neo4j Public Repository" at "http://m2.neo4j.org",
       resolvers += "fakod-snapshots" at "https://raw.github.com/FaKod/fakod-mvn-repo/master/snapshots",
-      libraryDependencies ++= Seq("junit" % "junit" % "4.7",
-                                  "org.neo4j" % "neo4j-scala" % "0.2.0-M2-SNAPSHOT",
-                                  "org.neo4j" % "neo4j-kernel" % neo4jVersion,
-                                  "org.neo4j" % "neo4j" % neo4jVersion,
-                                  "org.neo4j" % "neo4j-lucene-index" % neo4jVersion,
-                                  "org.neo4j" % "neo4j-shell" % neo4jShellVersion,
-                                  "org.neo4j" % "neo4j-rest-graphdb" % neo4jRestVersion,
-                                  "org.neo4j" % "neo4j-cypher" % neo4jCypherVersion,
+      libraryDependencies ++= Seq("junit" % "junit" % "4.4",
+                                  "org.jsoup" % "jsoup" % "1.6.1",
+                                  "commons-io" % "commons-io" % "2.1",
+                                  "joda-time" % "joda-time" % "2.0",
+                                  "org.joda" % "joda-convert" % "1.1",
                                   "org.scala-lang" % "scala-compiler" % "2.9.2")
 
     )
---
+++ src/main/scala/dk/test/test.scala
@@ -0,0 +1,16 @@
+package dk.test
+
+import dk.atp.api.tournament.GenericTournamentAtpApi
+import dk.atp.api._
+
+object MatchesLoadSimple extends App {
+  //Increase 3000ms connection timeout, if loading tennis matches fails. 
+  //There are dozens of requests sent to atp website, so
+  //even with a higher timeout the loadMatches method may sometimes fail. 
+  //With a high speed internet connection, loading all tennis matches
+  //for 2011 takes between 3-6 seconds.
+  var tournamentApi: GenericTournamentAtpApi = new GenericTournamentAtpApi(360000)
+  val genericATPMatchesLoader = new GenericATPMatchesLoader(tournamentApi)
+  val matches =  genericATPMatchesLoader.loadMatches(2011)
+  CSVATPMatchesLoader.toCSVFile(matches, "/tmp/matches.csv")
+}
