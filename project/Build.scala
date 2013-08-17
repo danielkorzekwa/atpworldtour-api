4ffb98f4599be70cbe813465ab944011730a3bae
Dinesh Weerapurage <xydinesh@gmail.com>
2013-08-16 23:09:38 -0400

    adding new build scala file

--- project/Build.scala
+++ project/Build.scala
@@ -19,8 +19,6 @@ object AtpWorldTourScalaBuild extends Build {
       version := "0.2",
       scalaVersion := "2.9.2",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-      resolvers += "Neo4j Public Repository" at "http://m2.neo4j.org",
-      resolvers += "fakod-snapshots" at "https://raw.github.com/FaKod/fakod-mvn-repo/master/snapshots",
       libraryDependencies ++= Seq("junit" % "junit" % "4.4",
                                   "org.jsoup" % "jsoup" % "1.6.1",
                                   "commons-io" % "commons-io" % "2.1",
@@ -31,5 +29,5 @@ object AtpWorldTourScalaBuild extends Build {
     )
   )
 }
- 
+
 
