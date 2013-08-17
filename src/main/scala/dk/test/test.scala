1c60d64d9ce866f61cb86d16e19bc8f6d822f324
Dinesh Weerapurage <xydinesh@gmail.com>
2013-08-16 23:00:17 -0400

    adding working version

--- src/main/scala/dk/test/test.scala
+++ src/main/scala/dk/test/test.scala
@@ -4,13 +4,13 @@ import dk.atp.api.tournament.GenericTournamentAtpApi
 import dk.atp.api._
 
 object MatchesLoadSimple extends App {
-  //Increase 3000ms connection timeout, if loading tennis matches fails. 
+  //Increase 3000ms connection timeout, if loading tennis matches fails.
   //There are dozens of requests sent to atp website, so
-  //even with a higher timeout the loadMatches method may sometimes fail. 
+  //even with a higher timeout the loadMatches method may sometimes fail.
   //With a high speed internet connection, loading all tennis matches
   //for 2011 takes between 3-6 seconds.
   var tournamentApi: GenericTournamentAtpApi = new GenericTournamentAtpApi(360000)
   val genericATPMatchesLoader = new GenericATPMatchesLoader(tournamentApi)
-  val matches =  genericATPMatchesLoader.loadMatches(2011)
-  CSVATPMatchesLoader.toCSVFile(matches, "/tmp/matches.csv")
+  val matches =  genericATPMatchesLoader.loadMatches(2013)
+  CSVATPMatchesLoader.toCSVFile(matches, "C:\\Devs\\matches_2013.csv")
 }
